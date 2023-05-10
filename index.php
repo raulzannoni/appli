b59996d55e57fe469f750112a7529b9b08e4fbb7
raul_zannoni <raul.zannnoni92@gmail.com>
2023-05-10 15:52:02 +0200

    index style

--- index.php
+++ index.php
@@ -53,15 +53,22 @@
             <p>
                 <input type="submit" name="submit" value="Ajouter le produit">
             </p>
+
+            <!-- Notification du nombre de produits ajoutés -->
+            <p id="nombreProducts">Nombre de produits ajouté: <?=$nb_products?></p>
+
+             <!-- Bouton pour changer la page -->
+            <button formaction='traitement.php?action=recap' method="post" id="btnRecap">Consulter le panier</button>
+
         </form>
     
-    <!-- Notification du nombre de produits ajoutés -->
-    <p>Nombre de produits ajouté: <?=$nb_products?></p>
 
-    <!-- Bouton pour changer la page -->
-    <form action='traitement.php?action=recap' method="post">
-        <button id="btnRecap">Consulter le récapitulatif</button>
-    </form>
+    
+
+   
+    
+
+
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
