61ee9b79f48169b51b56fe141f77d83402ea7778
raul.zannoni <raul.zannoni92@gmail.com>
2023-05-11 14:40:38 +0200

    add file input

--- index.php
+++ index.php
@@ -4,8 +4,18 @@
     //la session correspondante à l'utilisateur
     session_start();
     ob_start();
-?>
-        <?php 
+
+    var_dump($_FILES);
+    var_dump($_POST);
+
+    if(isset($_FILES['file'])){
+        $tmpName = $_FILES['file']['tmp_name'];
+        $name = $_FILES['file']['name'];
+        $size = $_FILES['file']['size'];
+        $error = $_FILES['file']['error'];
+    }
+
+
         if(isset($_SESSION['message'])){
             echo $_SESSION['message'];
             unset($_SESSION['message']);
@@ -29,28 +39,35 @@
                 <!-- action: le fichier à atteindre lorsque l'utilisateur soumettra le formulaire -->
                 <!-- method: quelle methode HTTP les données du formulaire seront transmises au serveur -->
                 <!-- POST: pour ne pas "polluer" l'URL avec les données du formulaire --> 
-                <form action="traitement.php?action=addProduct" method="post">
+                <form action="traitement.php?action=addProduct" method="post" enctype="multipart/form-data">
                     <p>
                         <label>
                             Nom du produit:
-                            <input type="text" name="name">
+                            <input id="inputText" type="text" name="name">
                         </label>
+                    </p>
+                    <p>
+                        <label for="file">
+                            Fichier:
+                        </label>
+                            <input id="inputFile" type="file" name="file">
                     </p>
                     <p>
                         <label>
                             Prix du produit:
-                            <input type="number" step= "any" name="price">
+                            <input id="inputText" type="number" step= "any" name="price">
                         </label>
                     </p>
                     <p>
                         <label>
                             Quantité desirée:
-                            <input type="number" name="qtt" value="1">
+                            <input id="inputText" type="number" name="qtt" value="1">
                         </label>
                     </p>
-                    <p>
-                        <input type="submit" name="submit" value="Ajouter le produit">
-                    </p>
+                    
+                    <!-- Bouton pour ajouter le produit -->
+                    <button id="btnAdd" formaction="traitement.php?action=addProduct" id="inputSubmit" type="submit" name="submit" >Ajouter le produit</button>
+                    
 
                     <!-- Notification du nombre de produits ajoutés -->
                     <p id="nombreProducts">Nombre de produits ajouté: <?=$nb_products?></p>
